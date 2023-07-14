90f6677c30d4106bd913f4c423642f7dc357dd8c
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-07-14 15:24:03 +0200

    Populate UI with data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import {Painting} from "@/data/types";
+import { Painting } from '@/data/types';
+
 async function getData(): Promise<Painting[]> {
   const res = await fetch('http://localhost:3000/data.json');
 
@@ -14,11 +15,27 @@ async function getData(): Promise<Painting[]> {
 export default async function Home() {
   const data = await getData();
 
-  console.log(data)
+  console.log(data);
 
-  return <main className="w-full">
-    <ul>
-      {data.map(painting => <li key={painting.name}>{painting.name}</li>)}
-    </ul>
-  </main>;
+  return (
+    <main className='w-full'>
+      <ul className='grid gap-10 sm:grid-cols-2 lg:grid-cols-4'>
+        {data.map((painting) => (
+          <li key={painting.name}>
+            <div className='relative'>
+              <img
+                src={painting.images.thumbnail}
+                alt={painting.name}
+                className='w-full'
+              />
+              <div className='absolute bottom-0 p-8 text-white'>
+                <h2 className='text-2xl'>{painting.name}</h2>
+                <h3 className='opacity-75 text-xs'>{painting.artist.name}</h3>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
