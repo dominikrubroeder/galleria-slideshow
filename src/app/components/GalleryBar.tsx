36ba2da2415de725300b8270b1d278157bb070a0
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-07-19 12:01:34 +0200

    Animate Slideshow progress

--- src/app/components/GalleryBar.tsx
+++ src/app/components/GalleryBar.tsx
@@ -1,8 +1,20 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
 export default function GalleryBar() {
   return (
     <div className='fixed right-0 left-0 bottom-0 w-full bg-white z-50'>
       <div className='relative h-0.5 w-full bg-[#E5E5E5]'>
-        <span className='absolute left-0 bg-black w-1/4 h-full'></span>
+        <motion.span
+          className='absolute left-0 bg-black h-full'
+          animate={{ width: ['0%', '100%'] }}
+          transition={{
+            ease: 'linear',
+            duration: 10,
+            delay: 0.4,
+          }}
+        ></motion.span>
       </div>
 
       <div className='flex gap-4 justify-between p-4'>
@@ -11,9 +23,15 @@ export default function GalleryBar() {
           <p className='text-sm'>Vincent Van Gogh</p>
         </div>
 
-        <div className='flex items-center gap-4'>
-          <button>Prev</button>
-          <button>Next</button>
+        <div>
+          <div>
+            <button>Stop</button>
+          </div>
+
+          <div className='flex items-center gap-4'>
+            <button>Prev</button>
+            <button>Next</button>
+          </div>
         </div>
       </div>
     </div>
