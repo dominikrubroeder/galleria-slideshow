e98b1803f4d22ec16233b42b0d5a4bbf0d358190
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-07-19 17:16:24 +0200

    Handle slideshow start from Home page

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,18 +1,23 @@
 'use client';
 
+import { usePathname, useRouter } from 'next/navigation';
 import { useGalleryContext } from '../GalleryContext';
 import Logo from './Logo';
 
 export default function Header() {
+  const router = useRouter();
+  const pathname = usePathname();
   const galleryCtx = useGalleryContext();
+
+  const handleClick = () => {
+    if (pathname === '/') router.push('/starry-night');
+    galleryCtx.setIsPlaying(true);
+  };
 
   return (
     <header className='py-10 w-full border-b flex justify-between gap-4'>
       <Logo />
-      <button
-        className='uppercase text-xs'
-        onClick={() => galleryCtx.setIsPlaying(true)}
-      >
+      <button className='uppercase text-xs' onClick={handleClick}>
         Start Slideshow
       </button>
     </header>
